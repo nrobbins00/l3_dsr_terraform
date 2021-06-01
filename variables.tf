91385a3618b8e2c63f24f49014dae725a7e60137
Nick Robbins <17030717+nrobbins00@users.noreply.github.com>
2021-06-01 08:30:34 -0500

    Add explanatory comments

--- variables.tf
+++ variables.tf
@@ -5,16 +5,16 @@ variable "avi_version" {}
 variable "avi_controller" {}
 variable "tier_1_vrf" {}
 variable "tier_2_vrf" {}
-variable "server_subnet" {}
-variable "front_end_subnet" {}
-variable "vs_ip" {}
-variable "t2_portgroup" {}
-variable "t1_portgroup" {}
-variable "front_end_subnet_mask" {}
-variable "server_subnet_mask" {}
+variable "server_subnet" {}  # subnet in dotted decimal for back end servers and SEs (ex. 10.10.10.0)
+variable "front_end_subnet" {} # subnet in dotted decimal for front end SEs (ex. 10.10.10.0)
+#variable "vs_ip" {} 
+variable "t2_portgroup" {} # name of vsphere port group for Tier 2
+variable "t1_portgroup" {} # name of vsphere port group for Tier 1
+variable "front_end_subnet_mask" {} # subnet mask in slash notation (integer, ex. 24)
+variable "server_subnet_mask" {} # subnet mask in slash notation (integer, ex. 24)
 variable "webserver1_ip" {}
 variable "webserver2_ip" {}
 variable "avi_cloud" {}
-variable "vip_portgroup" {}
-variable "vip_subnet" {}
-variable "vip_subnet_mask" {}
+variable "vip_portgroup" {} ## name of vsphere port group for portgroup associated with VIP range
+variable "vip_subnet" {} # subnet in dotted decimal for VIPs (ex. 10.10.10.0)
+variable "vip_subnet_mask" {} # subnet mask in slash notation (integer, ex. 24)
